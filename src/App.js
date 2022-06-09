3f225c38c60fc18be6cc796fcef568175bcbb06f
Charles Bartlett <ownageunit@gmail.com>
2022-06-09 16:48:16 -0400

    logic done just needs style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  // randomNumber between 0 and 256
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 256)
+  }
+
+  // empty array to store colors numbers in
+  const colorsArray = []
+  const [selectedColor, selectedColorUpdate] = useState("0, 0, 0")
+  const handleClick = ({red, green, blue}) => {
+    selectedColorUpdate(`${red}, ${green}, ${blue}`)
+  }
+
+  const [newColorDivs, setNewColorDivs] = useState("no colors yet")
+  // let newColorDivs = null
+  const newColorsArray = []
+  
+  const genNewColors = () => {
+    
+    for (let index = 0; index < 64; index++) {
+      const colors = {red: randomNumber(), 
+                green: randomNumber(),
+                blue: randomNumber()
+              }
+      newColorsArray.push(colors)
+    }
+
+
+    const newColorDivs = newColorsArray.map(({red, green, blue}, index) => (
+      <div 
+      style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+              width: "25px",
+              height: "25px"
+      }}
+      onClick={()=>{handleClick({red, green, blue})}}
+      key={index}
+      />
+    ))
+
+    setNewColorDivs(newColorDivs)
+  }
+
+
+  for (let index = 0; index < 5; index++) {
+    const colors = {red: randomNumber(), 
+              green: randomNumber(),
+              blue: randomNumber()
+            }
+    colorsArray.push(colors)
+  }
+
+  // maps through color values array and creates a div with RGB values
+  const colorDivs = colorsArray.map(({red, green, blue}, index) => (
+    <div 
+    style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+            width: "50px",
+            height: "50px"
+    }}
+    onClick={()=>{handleClick({red, green, blue})}}
+    key={index}
+    />
+  ))
+
+  const [keepColor1, updateKeepColor1] = useState (0, 0, 0)
+  const [keepColor2, updateKeepColor2] = useState (0, 0, 0)
+  const [keepColor3, updateKeepColor3] = useState (0, 0, 0)
+
+  const colorKeep1 = (event) => {
+    event.preventDefault()
+    updateKeepColor1(selectedColor)
+  }
+  const colorKeep2 = (event) => {
+    event.preventDefault()
+    updateKeepColor2(selectedColor)
+  }
+  const colorKeep3 = (event) => {
+    event.preventDefault()
+    updateKeepColor3(selectedColor)
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Color Picker</h1>
+    <div id='displayColors'>
+    {/* {colorDivs} */}
+    {newColorDivs}
+    </div>
+    <button onClick={genNewColors}>Generate New Colors</button>
+    <div id='selectedColor'>
+      <h1>Selected Color</h1>
+      <div 
+    style={{backgroundColor: `rgb(${selectedColor})`,
+            width: "50px",
+            height: "50px",
+            borderStyle: "solid",
+            borderColor: "black"
+      }}
+      />
+    </div>
+    <div id='keptColors'>
+      <div 
+      style={{backgroundColor: `rgb(${keepColor1})`,
+            width: "100px",
+            height: "100px",
+            borderStyle: "solid",
+            borderColor: "black"
+      }}
+      onClick={colorKeep1}
+      />
+      <div 
+    style={{backgroundColor: `rgb(${keepColor2})`,
+            width: "100px",
+            height: "100px",
+            borderStyle: "solid",
+            borderColor: "black"
+      }}
+      onClick={colorKeep2}
+      />
+      <div 
+      style={{backgroundColor: `rgb(${keepColor3})`,
+            width: "100px",
+            height: "100px",
+            borderStyle: "solid",
+            borderColor: "black"
+      }}
+      onClick={colorKeep3}
+      />
+      </div>
     </div>
   );
 }
